b741e4e9637d69996fec8bec12628fbd180baf1a
egen <christeigentheodore@gmail.com>
2023-08-12 23:14:46 +0700

    feat: additional barchart, fixed bug, and sort the barchart

--- barata_frontend/src/components/chart/bar.tsx
+++ barata_frontend/src/components/chart/bar.tsx
@@ -1,4 +1,4 @@
-import { Card, Title, BarChart, Subtitle } from "@tremor/react";
+import { Card, Title, BarChart, Subtitle, Grid, Col } from "@tremor/react";
 import React from 'react';
 
 interface Item {
@@ -32,7 +32,7 @@ const MyBarChart: React.FC<MyBarChartProps> = ({ source, columns, title, shuffle
       if (isNaN(totalPrice) || isNaN(duration)) {
         console.log('NaN values detected:', item);
       }
-      
+
       const existingCsId = acc.findIndex((accItem: Item) => accItem.csId === csId);
       if (existingCsId !== -1) {
         acc[existingCsId].totalPrice += totalPrice;
@@ -40,35 +40,58 @@ const MyBarChart: React.FC<MyBarChartProps> = ({ source, columns, title, shuffle
       } else {
         acc.push({ csId, totalPrice, duration });
       }
-  
+
       return acc;
     }, []);
   };
-  
+
   const data = groupAndSumData(dataToDisplay);
 
   const chartData = data.map((item) => ({
     name: item.csId,
     "Total Sales": parseFloat(item.totalPrice.toFixed(2)),
+    "Total Duration": item.duration,
   }));
-  console.log(data);
+
+  const salesChartData = chartData.slice().sort((a, b) => b["Total Sales"] - a["Total Sales"]);
+  const durationdChartData = chartData.slice().sort((a, b) => b["Total Duration"] - a["Total Duration"]);
+
 
   return (
-    <Card>
-      <Title>{title}</Title>
-      <Subtitle>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
-      </Subtitle>
-      <BarChart
-        className="mt-6"
-        data={chartData}
-        index="name"
-        categories={["Total Sales"]}
-        colors={["cyan"]}
-        valueFormatter={dataFormatter}
-        yAxisWidth={48}
-      />
-    </Card>
+    <Grid numItemsSm={2} numItemsLg={2} numItemsMd={2} className="gap-6">
+      <Col numColSpan={1} numColSpanLg={1}>
+        <Card>
+          <Title>{title}</Title>
+          <Subtitle>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+          </Subtitle>
+          <BarChart
+            className="mt-6"
+            data={salesChartData}
+            index="name"
+            categories={["Total Sales"]}
+            colors={["cyan"]}
+            valueFormatter={dataFormatter}
+            yAxisWidth={48}
+          />
+        </Card>
+      </Col>
+      <Card>
+        <Title>Top Charging Station by Total Duration</Title>
+        <Subtitle>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+        </Subtitle>
+        <BarChart
+          className="mt-6"
+          data={durationdChartData}
+          index="name"
+          categories={["Total Duration"]}
+          colors={["amber"]}
+          valueFormatter={dataFormatter}
+          yAxisWidth={48}
+        />
+      </Card>
+    </Grid>
   );
 };
 
