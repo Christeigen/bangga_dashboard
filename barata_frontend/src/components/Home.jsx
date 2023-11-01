d55a7c8497ce9e8182eefa156e030326df1a74a4
egen <christeigentheodore@gmail.com>
2023-11-01 15:58:37 +0700

    delete background asset

--- barata_frontend/src/components/Home.jsx
+++ barata_frontend/src/components/Home.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import BannerBackground from "../Assets/home-banner-background.png";
-import BannerImage from "../Assets/home-banner-image.png";
 import Navbar from "./Navbar";
 import { FiArrowRight } from "react-icons/fi";
 
@@ -9,9 +7,6 @@ const Home = () => {
     <div className="home-container">
       <Navbar />
       <div className="home-banner-container">
-        <div className="home-bannerImage-container">
-          <img src={BannerBackground} alt="" />
-        </div>
         <div className="home-text-section">
           <h1 className="primary-heading">
             Electric Vehicle Charging Stations
@@ -24,9 +19,6 @@ const Home = () => {
           <button style={{ backgroundColor: 'blue', color: 'white' }} className="secondary-button">
             Order Now <FiArrowRight />{" "}
           </button>
-        </div>
-        <div className="home-image-section">
-          <img src={BannerImage} alt="" />
         </div>
       </div>
     </div>
