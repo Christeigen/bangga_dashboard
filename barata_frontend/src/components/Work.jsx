5d0f672bdf3c9e798528adf442a598a683a61ee8
egen <christeigentheodore@gmail.com>
2023-11-01 16:07:19 +0700

    delete work images

--- barata_frontend/src/components/Work.jsx
+++ barata_frontend/src/components/Work.jsx
@@ -1,22 +1,16 @@
 import React from "react";
-import PickMeals from "../Assets/money.jpeg";
-import ChooseMeals from "../Assets/electric.jpeg";
-import DeliveryMeals from "../Assets/charging-station.jpg";
 
 const Work = () => {
   const workInfoData = [
     {
-      image: PickMeals,
       title: "Cheap",
       text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et.",
     },
     {
-      image: ChooseMeals,
       title: "Fast and Efficient",
       text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et ",
     },
     {
-      image: DeliveryMeals,
       title: "Easy to Access",
       text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et lorem ipsum",
     },
@@ -34,9 +28,6 @@ const Work = () => {
       <div className="work-section-bottom">
         {workInfoData.map((data) => (
           <div className="work-section-info" key={data.title}>
-            <div className="info-boxes-img-container">
-              <img src={data.image} alt="" />
-            </div>
             <h2>{data.title}</h2>
             <p>{data.text}</p>
           </div>
