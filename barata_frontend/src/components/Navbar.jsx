f22529f3c3441479284a88ef7bc73ad215d41379
egen <christeigentheodore@gmail.com>
2023-11-01 13:46:24 +0700

    Revisi_0

--- barata_frontend/src/components/Navbar.jsx
+++ barata_frontend/src/components/Navbar.jsx
@@ -11,46 +11,34 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import HomeIcon from "@mui/icons-material/Home";
-import InfoIcon from "@mui/icons-material/Info";
-import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
-import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
-import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import BanggaLogo from "../Assets/BANGGA-EVCS.png";
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
   const menuOptions = [
     {
-      text: "Home",
-      icon: <HomeIcon />,
+      text: "Home"
     },
     {
-      text: "About",
-      icon: <InfoIcon />,
+      text: "Tentang Kami"
     },
     {
-      text: "Testimonials",
-      icon: <CommentRoundedIcon />,
+      text: "Produk"
     },
     {
-      text: "Contact",
-      icon: <PhoneRoundedIcon />,
-    },
-    {
-      text: "Cart",
-      icon: <ShoppingCartRoundedIcon />,
+      text: "Aplikasi"
     },
   ];
   return (
     <nav>
       <div className="nav-logo-container">
-        <img src={Logo} alt="" />
+        <img src={BanggaLogo} alt="" />
       </div>
       <div className="navbar-links-container">
         <a href="">Home</a>
-        <a href="">About</a>
-        <a href="">Testimonials</a>
-        <a href="">Contact</a>
+        <a href="">Tentang kami</a>
+        <a href="">Produk</a>
+        <a href="">Aplikasi</a>
         <button className="primary-button">Download Now</button>
       </div>
       <div className="navbar-menu-container">
