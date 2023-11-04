42857a49e8eab94dc17e5dfed3f1c5ee3a97aeda
unknown <projectwilsen@gmail.com>
2023-11-04 15:26:50 +0700

    add

--- barata_backend/core/manage.py
+++ barata_backend/core/manage.py
@@ -6,7 +6,14 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+    try:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    except:
+        os.environ['DJANGO_SETTINGS_MODULE']
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
