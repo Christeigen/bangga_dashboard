1976a0f9e951268fc461505e5f4479743fea8ab4
unknown <projectwilsen@gmail.com>
2023-11-04 11:01:11 +0700

    add static

--- barata_backend/core/core/settings.py
+++ barata_backend/core/core/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", 
-    "http://62.72.30.54/"
+    "http://62.72.30.54"
 ]
 
 
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- barata_backend/core/core/wsgi.py
+++ barata_backend/core/core/wsgi.py
@@ -10,6 +10,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
