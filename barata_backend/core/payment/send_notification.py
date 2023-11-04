b880740a3ea98d146078d9c5185149b7dfaa03f7
unknown <projectwilsen@gmail.com>
2023-11-04 22:21:01 +0700

    add json

--- barata_backend/core/payment/send_notification.py
+++ barata_backend/core/payment/send_notification.py
@@ -2,7 +2,7 @@
 # These registration tokens come from the client FCM SDKs.
 import firebase_admin
 from firebase_admin import credentials, messaging
-cred = credentials.Certificate("D:/pembayaran_django/firebase/fireapp/chargingstation-17519-firebase-adminsdk-oxgx9-71e7688b45.json")
+cred = credentials.Certificate("C:/Users/Geraldus Wilsen/Documents/ProjectBarata/barata_dashboard/barata_backend/core/chargingstation-17519-firebase-adminsdk-oxgx9-71e7688b45.json")
 firebase_admin.initialize_app(cred)
 
 def send_notif(registration_tokens, title, body):
