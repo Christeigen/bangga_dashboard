06ca2fe1f78a6eadacf60efe17b76986ab90f0ac
unknown <projectwilsen@gmail.com>
2023-11-24 12:58:49 +0700

    solve navbar bug

--- barata_frontend/src/components/shared/company_profile/Navbar.jsx
+++ barata_frontend/src/components/shared/company_profile/Navbar.jsx
@@ -1,18 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Link,NavLink, useLocation } from "react-router-dom";
 import close from '/src/assets/close.svg';
 import bangga_logo from '/src/assets/bangga_logo.png';
 import menu from '/src/assets/menu.svg';
 import { navLinks } from "/src/constants/index";
 
+
 export default function Navbar({ color }) {
 
-  const [active, setActive] = useState("Home");
   const [toggle, setToggle] = useState(false);
-
-  const handleNavLinkClick = (event, title) => {
-    setActive(title);
-    event.preventDefault();
-  };
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
@@ -20,15 +16,17 @@ export default function Navbar({ color }) {
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
-          <li
-            key={nav.id}
-            className={`font-poppins cursor-pointer text-[16px] 
-                    ${active === nav.title ? "text-sky-900 font-black" : color == "white" ? "text-white" : "text-black"} 
-                    ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
-            onClick={() => setActive(nav.title)}
-            // onClick={(event) => handleNavLinkClick(event, nav.title)}
-          >
-            <a href={`${nav.id}`}>{nav.title}</a>
+          <li key={nav.id}>
+            <NavLink
+              to={`/${nav.id}`}
+              className={({ isActive}) => 
+              `font-poppins cursor-pointer text-[16px] 
+              ${isActive ? "text-sky-900 font-black" : color === "white" ? "text-white" : "text-black"} 
+              ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`
+              }
+            >
+              {nav.title}
+            </NavLink>
           </li>
         ))}
       </ul>
@@ -45,70 +43,9 @@ export default function Navbar({ color }) {
           className={`${!toggle ? "hidden" : "flex"
             } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
-
         </div>
       </div>
     </nav>
-
-    // <div>Navbar</div>
-  )
+  );
 }
 
-
-
-
-// const Navbar = () => {
-//   const [active, setActive] = useState("Home");
-//   const [toggle, setToggle] = useState(false);
-
-//   return (
-//     <nav className="w-full flex py-6 justify-between items-center navbar">
-//       <img src={bangga_logo} alt="hoobank" className="w-[124px] h-[32px]" />
-
-//       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-//         {navLinks.map((nav, index) => (
-//           <li
-//             key={nav.id}
-//             className={`font-poppins font-normal cursor-pointer text-[16px] ${
-//               active === nav.title ? "text-white" : "text-dimWhite"
-//             } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
-//             onClick={() => setActive(nav.title)}
-//           >
-//             <a href={`#${nav.id}`}>{nav.title}</a>
-//           </li>
-//         ))}
-//       </ul>
-
-//       <div className="sm:hidden flex flex-1 justify-end items-center">
-//         <img
-//           src={toggle ? close : menu}
-//           alt="menu"
-//           className="w-[28px] h-[28px] object-contain"
-//           onClick={() => setToggle(!toggle)}
-//         />
-
-//         <div
-//           className={`${
-//             !toggle ? "hidden" : "flex"
-//           } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
-//         >
-//           <ul className="list-none flex justify-end items-start flex-1 flex-col">
-//             {navLinks.map((nav, index) => (
-//               <li
-//                 key={nav.id}
-//                 className={`font-poppins font-medium cursor-pointer text-[16px] ${
-//                   active === nav.title ? "text-white" : "text-dimWhite"
-//                 } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
-//                 onClick={() => setActive(nav.title)}
-//               >
-//                 <a href={`#${nav.id}`}>{nav.title}</a>
-//               </li>
-//             ))}
-//           </ul>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
