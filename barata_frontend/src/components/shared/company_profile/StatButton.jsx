88e9400c556cd2b449af1db3f67804c7ca10c133
unknown <projectwilsen@gmail.com>
2023-11-30 18:52:44 +0700

    add

--- barata_frontend/src/components/shared/company_profile/StatButton.jsx
+++ barata_frontend/src/components/shared/company_profile/StatButton.jsx
@@ -1,25 +1,131 @@
-export default function StatButton(status) {
-    console.log("status", status.status)
-    if (status.status == 0){
-        return (
-            <div className="flex flex-row gap-2 justify-center">
-                <button className="px-2 py-2 text-green-900 text-sm bg-green-200 rounded-lg flex flex-row">Accept</button>
-                <button className="px-2 py-2 text-red-900 text-sm bg-red-200 rounded-lg flex flex-row">Reject</button>
-            </div>
-        )
+// import { useState } from 'react';
+// import axios from 'axios';
+
+// export default function StatButton({ customer_id, withdraw_id, status }) {
+//   const [isSubmitting, setIsSubmitting] = useState(false);
+
+//   const handleButtonClick = async (customer_id, withdraw_id, verif_status) => {
+//     if (isSubmitting) return;
+
+//     setIsSubmitting(true);
+
+//     try {
+//       const response = await axios.post('http://127.0.0.1:8000/api/updateverifstatus/', { customer_id, withdraw_id, verif_status });
+
+//       if (response.status === 200) {
+//         console.log('POST request successful');
+//       } else {
+//         console.error('POST request failed');
+//       }
+//     } catch (error) {
+//       console.error('Error during POST request:', error);
+//     } finally {
+//       setIsSubmitting(false);
+//     }
+//   };
+
+//   if (status === 0) {
+//     return (
+//       <div className="flex flex-row gap-2 justify-center">
+//         <button
+//           className="px-2 py-2 text-green-900 text-sm bg-green-200 rounded-lg flex flex-row"
+//           onClick={() => handleButtonClick(customer_id, withdraw_id, 100)} // Change the status accordingly
+//         >
+//           Accept
+//         </button>
+//         <button
+//           className="px-2 py-2 text-red-900 text-sm bg-red-200 rounded-lg flex flex-row"
+//           onClick={() => handleButtonClick(customer_id, withdraw_id, 200)} // Change the status accordingly
+//         >
+//           Reject
+//         </button>
+//       </div>
+//     );
+//   } else if (status === 100) {
+//     return (
+//       <div className="flex flex-row gap-2 justify-center">
+//         <button className="px-2 py-2 text-white text-sm bg-zinc-500 rounded-lg flex flex-row" disabled>
+//           Accepted
+//         </button>
+//       </div>
+//     );
+//   } else if (status === 200) {
+//     return (
+//       <div className="flex flex-row gap-2 justify-center">
+//         <button className="px-2 py-2 text-white text-sm bg-zinc-500 rounded-lg flex flex-row" disabled>
+//           Rejected
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+import { useState } from 'react';
+import axios from 'axios';
+
+export default function StatButton({ customer_id, withdraw_id, status }) {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleButtonClick = async (customer_id, withdraw_id, verif_status) => {
+    if (isSubmitting) return;
+
+    setIsSubmitting(true);
+
+    try {
+
+        const userConfirmed = window.confirm(`Are you sure you want to update the status to ${verif_status === 100 ? 'Accepted' : 'Rejected'}?`);
+
+        if (!userConfirmed) {
+          return;
+        }
+
+      const response = await axios.post('http://127.0.0.1:8000/api/updateverifstatus/', { customer_id, withdraw_id, verif_status });
+
+      if (response.status === 200) {
+        window.location.reload();
+      } else {
+        window.alert('Update failed');
+      }
+    } catch (error) {
+      window.alert(`Error during POST request: ${error.message}`);
+    } finally {
+      setIsSubmitting(false);
     }
-    else if (status.status == 100) {
-        return (
-            <div className="flex flex-row gap-2 justify-center">
-                <button className="px-2 py-2 text-white text-sm bg-zinc-500 rounded-lg flex flex-row">Accepted</button>
-            </div>
-        )
-    }
-    else if (status.status == 200) {
-        return (
-            <div className="flex flex-row gap-2 justify-center">
-                <button className="px-2 py-2 text-white text-sm bg-zinc-500 rounded-lg flex flex-row">Rejected</button>
-            </div>
-        )
-    }
+  };
+
+  if (status === 0) {
+    return (
+      <div className="flex flex-row gap-2 justify-center">
+        <button
+          className="px-2 py-2 text-green-900 text-sm bg-green-200 rounded-lg flex flex-row"
+          onClick={() => handleButtonClick(customer_id, withdraw_id, 100)} // Change the status accordingly
+        >
+          Accept
+        </button>
+        <button
+          className="px-2 py-2 text-red-900 text-sm bg-red-200 rounded-lg flex flex-row"
+          onClick={() => handleButtonClick(customer_id, withdraw_id, 200)} // Change the status accordingly
+        >
+          Reject
+        </button>
+      </div>
+    );
+  } else if (status === 100) {
+    return (
+      <div className="flex flex-row gap-2 justify-center">
+        <button className="px-2 py-2 text-white text-sm bg-zinc-500 rounded-lg flex flex-row" disabled>
+          Accepted
+        </button>
+      </div>
+    );
+  } else if (status === 200) {
+    return (
+      <div className="flex flex-row gap-2 justify-center">
+        <button className="px-2 py-2 text-white text-sm bg-zinc-500 rounded-lg flex flex-row" disabled>
+          Rejected
+        </button>
+      </div>
+    );
+  }
 }
+
